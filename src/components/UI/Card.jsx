c7eb404682fce27a1555b07b48952d90b663ed29
Boris <47457848+boris-chb@users.noreply.github.com>
2021-10-10 20:34:43 +0100

    Initial commit

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,9 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const { children } = props;
+  const classes = `card ${props.className}`;
+  return <div className={classes}>{children}</div>;
+};
+
+export default Card;
